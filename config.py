0c0d16c3b3e11c15e0dccd15347d743f33bc9fcb
Lastonetry <alwaysoptimisticfutureiitian@gmail.com>
2024-08-04 23:53:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from os import environ 
 
 class Config:
-    OWNER_ID
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6970918221').split()]
     BOT_TOKEN = environ.get("BOT_TOKEN", "7360899365:AAG5PF3VpqCY5cOq6USO79AIUYgJYdD_VBA")
